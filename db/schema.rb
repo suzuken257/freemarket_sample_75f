f9ea6860f6ca65a4f60da47c3de762d0ad278165
digitmore <64125165+digitmore@users.noreply.github.com>
2020-05-25 18:02:44 +0900

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_102738) do
+ActiveRecord::Schema.define(version: 2020_05_22_054204) do
 
   create_table "deliver_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 2020_05_21_102738) do
     t.string "apartment_address"
     t.bigint "telephone"
     t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
