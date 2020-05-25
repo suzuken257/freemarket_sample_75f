e3bbe89ddf7273b084a313ac54367f8b158781b6
sazanca <moriya.kz817@gmail.com>
2020-05-25 23:30:42 +0900

    add on the way

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
 class Item < ApplicationRecord
+  has_many :comments
+  has_many :item_images
+  belongs_to :user
+  belongs_to :category
+
+  validates :name, presence: true, unless: :image?
+  
+  def show_last_items
+    if (last_item = item.last).present?
+      if last_item.content?
+        last_item.content
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
 end
