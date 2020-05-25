859b0d150bfb24500d749752254a41d359915ace
suzuken257 <60838938+suzuken257@users.noreply.github.com>
2020-05-25 20:03:32 +0900

    active_hash

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,15 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item=Item.new
+  end
+  def create
+    @item=Item.new(item_params)
+    Item.create
+    redirect_to "/"
+  end
+  private
+  def item_params
+    params.require(item).permit(:name,:introduction,:shipping_area_from,:price).merge(user_id: current_user.id)
   end
 end
