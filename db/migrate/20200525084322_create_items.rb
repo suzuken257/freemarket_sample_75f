d40d37350cd71aee0019509732e04db8fd87b4f1
sazanca <moriya.kz817@gmail.com>
2020-05-25 15:56:20 +0900

    add dbtable
    add itemstable,categorytable

---
+++ db/migrate/20200525084322_create_items.rb
@@ -0,0 +1,18 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name,                            null:false
+      t.integer :price,                          null:false
+      t.text :introduction,                      null:false
+      t.string :brand
+      t.integer :size
+      t.string :item_status,                     null:false
+      t.string :Shipping_fee_burden,             null:false
+      t.string :Shipping_area_from,              null:false
+      t.string :Estimated_shipping_date,         null:false
+      t.references :user, foreign_key: true
+      t.references :category_id, foreign_key: true
+      t.timestamps
+    end
+  end
+end
