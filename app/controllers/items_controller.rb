5b5e53276d41eed0fb0fe545119f065f7bda6f6e
suzuken257 <60838938+suzuken257@users.noreply.github.com>
2020-05-25 20:39:10 +0900

    エラーハンドリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,8 +7,11 @@ class ItemsController < ApplicationController
   end
   def create
     @item=Item.new(item_params)
-    @item.save
-    redirect_to root_path
+    if @item.save
+      redirect_to root_path
+    else
+      render "new"
+    end
   end
   private
   def item_params
