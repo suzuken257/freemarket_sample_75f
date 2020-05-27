e0c82b386a67ffb4c3ece812878378123a2ed094
sazanca <moriya.kz817@gmail.com>
2020-05-27 18:38:50 +0900

    add
    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only:[:index, :new, :show]
-  #   resources :item_image, only:[:index]
-  # end
+  resources :items, only:[:index, :new, :show, :create]
   resources :deliver_addresses, only:[:new, :create]
   resources :users, only: :show
   resources :credit_cards, only: [:new, :create, :edit, :update]
