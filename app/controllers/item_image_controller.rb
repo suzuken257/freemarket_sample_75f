d665d5cd25bfcf94b3186edc7de8a1c8912a4e41
sazanca <moriya.kz817@gmail.com>
2020-05-27 14:48:53 +0900

    fix

--- app/controllers/item_image_controller.rb
+++ app/controllers/item_image_controller.rb
@@ -1,11 +1,21 @@
 class ItemImageController < ApplicationController
   
   def index
-    @items = Item.includes(:images).order('created_at DESC')
+    # @items = Item.includes(:images).order('created_at DESC')
+  end
+
+  def create
+    @item=Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
   end
   
   private
 
-  def images_params
-    params.require(:item).permit(:name, :price, images_attributes: [:src])
+  def item_params
+    params.require(:item).permit(images_attributes: [:src])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :items, only:[:index, :new, :show]
+  #   resources :item_image, only:[:index]
+  # end
   resources :deliver_addresses, only:[:new, :create]
   resources :users, only: :show
   resources :credit_cards, only: [:new, :create, :edit, :update]
