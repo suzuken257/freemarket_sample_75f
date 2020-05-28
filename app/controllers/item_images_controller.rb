7860e91fcdd751d1abac1b3e55ab90fb8a9482c6
takumi <akaranki4@gmail.com>
2020-05-28 19:34:05 +0900

    コメントアウトなどの修正

--- app/controllers/item_images_controller.rb
+++ app/controllers/item_images_controller.rb
@@ -1,16 +1,2 @@
 class ItemImagesController < ApplicationControlle
-#   def create
-#     @item=ItemImage.new(item_image_params)
-#     if @item.save
-#       redirect_to items_path
-#     else
-#       render :new
-#     end
-#   end
-  
-#   private
-
-#   def item_image_params
-#     params.require(:item_image).permit(items_images_attributes: [:src])
-#   end
-# end
+end
