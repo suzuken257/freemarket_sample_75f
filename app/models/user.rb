e4d85945e61530056898f900a155d257a3162a0a
suzuken257 <60838938+suzuken257@users.noreply.github.com>
2020-05-29 19:11:45 +0900

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ApplicationRecord
   has_one :deliver_address, dependent: :destroy
   has_one :credit_card, dependent: :destroy
   has_many :items, dependent: :destroy
+
+  validates :nickname, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }}
+
 end
