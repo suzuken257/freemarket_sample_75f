7d72a010b4b080f5d52d03e61ef7db97714b239f
takumi <akaranki4@gmail.com>
2020-05-31 14:04:45 +0900

    comit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_item, only: [:show,:edit, :update, :destroy, :purchase_confirmation]
+  before_action :set_item, only: [:show,:edit, :update, :destroy, :purchase_confirmation, :buy]
 
   require 'payjp'
   
