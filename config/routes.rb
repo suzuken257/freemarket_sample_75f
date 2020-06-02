a1a8a1fc49f45619d130cb74e17fadf83fba5752
sazanca <moriya.kz817@gmail.com>
2020-06-02 17:55:27 +0900

    Revert "Merge pull request #16 from suzuken257/category_function"
    This reverts commit 7bf72543adc0b1d4ff45c29276cf07d014351360, reversing
    changes made to 06680c81370cde9dfa90732baa47802f762ececb.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :items do
-    collection do
-      get 'get_category_children', defaults: { format: 'json' }
-      get 'get_category_grandchildren', defaults: { format: 'json' }
-    end
     member do
       get :purchase_confirmation
       post :buy
