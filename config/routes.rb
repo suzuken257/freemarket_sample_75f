51d0ad633d4811ea94a045958e2bab0ad5b5b28e
digitmore <64125165+digitmore@users.noreply.github.com>
2020-06-09 20:53:59 +0900

    インデント修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-  omniauth_callbacks: 'users/omniauth_callbacks',
-  registrations: 'users/registrations'
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
   }
   root 'items#index'
   resources :items do
