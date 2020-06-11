352e973e888e1e2a77f315c233688c986bf4aba4
sazanca <moriya.kz817@gmail.com>
2020-06-11 18:48:26 +0900

    コンフリクト修正

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,8 +1,4 @@
 class ItemImage < ApplicationRecord
   mount_uploader :src, ImageUploader
-<<<<<<< Updated upstream
   belongs_to :item, inverse_of: :item_images
-=======
-  belongs_to :item, nverse_of: :item_images
->>>>>>> Stashed changes
 end
